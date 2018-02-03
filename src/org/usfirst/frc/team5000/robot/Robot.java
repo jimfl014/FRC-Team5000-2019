d70737434b79af240b7f87d336b64b907d54d4ab
Peter Heitman <peter@heitman.us>
2018-02-03 10:38:00 -0500

    Add in the autonomous framework from 2017. Need to customize it for this year's challenge

--- src/org/usfirst/frc/team5000/robot/Robot.java
+++ src/org/usfirst/frc/team5000/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SerialPort.Port;
@@ -22,11 +21,116 @@ import edu.wpi.first.wpilibj.SerialPort.Port;
  * directory.
  */
 public class Robot extends IterativeRobot {
+    
+	// Enums
+	static enum MotorState {
+		Stopped, Forward, Reverse
+	};
+
+	static enum DriveState {
+		Stopped, Forward, Reverse, Left, Right, Turn
+	};
+
+	static enum AutoStep {
+		Start, Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Step9, Step10, Stop;
+
+		public AutoStep next() {
+			switch (this) {
+			case Start:
+				return Step1;
+			case Step1:
+				return Step2;
+			case Step2:
+				return Step3;
+			case Step3:
+				return Step4;
+			case Step4:
+				return Step5;
+			case Step5:
+				return Step6;
+			case Step6:
+				return Step7;
+			case Step7:
+				return Step8;
+			case Step8:
+				return Step9;
+			case Step9:
+				return Step10;
+			default:
+				return Stop;
+			}
+		}
+
+		public String toString() {
+			switch (this) {
+			case Start:
+				return "Start";
+			case Step1:
+				return "Step1";
+			case Step2:
+				return "Step2";
+			case Step3:
+				return "Step3";
+			case Step4:
+				return "Step4";
+			case Step5:
+				return "Step5";
+			case Step6:
+				return "Step6";
+			case Step7:
+				return "Step7";
+			case Step8:
+				return "Step8";
+			case Step9:
+				return "Step9";
+			case Step10:
+				return "Step10";
+			default:
+				return "Stop";
+			}
+		}
+	};
+
+	// Constants
+
+	static final double FORWARD_WINCH_SPEED = -0.7;
+	static final double REVERSE_WINCH_SPEED = 0.6;
+	static final double FORWARD_DOOR_SPEED = 0.3; // 2/15 was 0.7
+	static final double REVERSE_DOOR_SPEED = -0.3; // 2/15 was -0.7
+	static final int DRIVE_DIRECTION_SWITCH_BUTTON = 2;
+	static final int OPEN_DOOR_BUTTON = 3;
+	static final int CLOSE_DOOR_BUTTON = 2;
+	static final int WINCH_UP_BUTTON = 1;
+	static final int WINCH_QUICK_RELEASE_BUTTON = 1;
+	static final long QUICK_RELEASE_TIME = 125;
+	static final String LEFT_AUTO = "Left";
+	static final String CENTER_AUTO = "Center";
+	static final String RIGHT_AUTO = "Right";
+	static final double Kp = 0.03;
+
+	// Variables
 	final String defaultAuto = "Default";
 	final String customAuto = "My Auto";
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
 
+	AutoStep autoStep = AutoStep.Start;
+	boolean initAutoStep = true;
+	double targetSpeed = 0;
+	long targetTime = 0;
+	double targetAngle = 0;
+	double targetTurningSpeed = 0;
+	double currentAngle = 0;
+	double angularDistance = 0;
+	boolean watchForReflectiveStrips = false;
+	DriveState driveState = DriveState.Stopped;
+	double baseDriveLevel = 0.05;
+	double baseTwistLevel = 0.15;
+	long beforeTurnTime = 2300;
+	long afterTurnTime = 2000;
+	long centerDriveTime = 1800;
+	boolean leftIRSensor, rightIRSensor;
+    
 	WPI_TalonSRX driveCimLF, driveCimLR, driveCimRF, driveCimRR;
 	Joystick driveJoystick;
 	HHJoystickButtons driveJoystickButtons;
@@ -54,6 +158,8 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto choices", chooser);
+
+		SmartDashboard.putString("Auto Step ", AutoStep.Start.toString());
 		
 		driveCimLF = new WPI_TalonSRX(0);
 		driveCimLR = new WPI_TalonSRX(1);
@@ -74,40 +180,6 @@ public class Robot extends IterativeRobot {
 		ahrs = new AHRS(Port.kUSB);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-	}
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
-	}
    /** Need to add teleopInit JF
     * 
     */
@@ -146,6 +218,405 @@ public class Robot extends IterativeRobot {
 		//actuator1.set(DoubleSolenoid.Value.kOff);
 	}
 	
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		autoSelected = chooser.getSelected();
+		System.out.println("Auto selected: " + autoSelected);
+
+		autoStep = AutoStep.Start;
+		SmartDashboard.putString("Auto Step ", autoStep.toString());
+		initializeForNextStep();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+
+            //		currentAngle = gyro.getAngle();
+
+            //		SmartDashboard.putString("Gyro Angle2 ", String.format("%.2f", currentAngle));
+
+            //		angularDistance = getAngularDistanceFromTarget(currentAngle, targetAngle);
+
+            //		SmartDashboard.putNumber("Target Angle ", targetAngle);
+            //		SmartDashboard.putNumber("D ", angularDistance);
+
+            //		leftIRSensor = irSensorLeft.get();
+            //		rightIRSensor = irSensorRight.get();
+
+            //		SmartDashboard.putString("Left IR  ", Boolean.toString(leftIRSensor));
+            //		SmartDashboard.putString("Right IR ", Boolean.toString(rightIRSensor));
+
+		boolean incrementStep = false;
+
+		if (targetTime == 0 && targetTurningSpeed == 0) {
+			incrementStep = true;
+		} else if (0 < targetTime && targetTime <= System.currentTimeMillis()) {
+			incrementStep = true;
+		} else if (targetTurningSpeed > 0 && Math.abs(angularDistance) < 1) {
+			incrementStep = true;
+		} else if (watchForReflectiveStrips && leftIRSensor && rightIRSensor) {
+			incrementStep = true;
+		}
+
+		if (incrementStep) {
+			initializeForNextStep();
+			autoStep = autoStep.next();
+
+			SmartDashboard.putString("Auto Step ", autoStep.toString());
+		}
+
+		switch (autoSelected) {
+		case LEFT_AUTO:
+			leftAutoProgram();
+			break;
+
+		default:
+		case CENTER_AUTO:
+			centerAutoProgram();
+			break;
+
+		case RIGHT_AUTO:
+			rightAutoProgram();
+			break;
+		}
+
+		doStep();
+	}
+
+	void leftAutoProgram() {
+
+		long driveTime = 0;
+
+		switch (autoStep) {
+		case Start:
+			break;
+
+		case Step1:
+			try {
+				driveTime = Long.parseLong(SmartDashboard.getString("Before Turn Time ", "bad"));
+			} catch (NumberFormatException e) {
+				driveTime = beforeTurnTime;
+			}
+
+			driveForward(0.3, driveTime);
+			break;
+
+		case Step2:
+			turnRight(0.10, 90);
+			watchForReflectiveStrips = true;
+			break;
+
+		case Step3:
+			try {
+				driveTime = Long.parseLong(SmartDashboard.getString("After Turn Time ", "bad"));
+			} catch (NumberFormatException e) {
+				driveTime = afterTurnTime;
+			}
+
+			driveForward(0.2, driveTime);
+			break;
+
+		case Step4:
+			pause(1000);
+			openDoors();
+			break;
+
+		case Step5:
+			driveReverse(0.3, 1000);
+
+		case Stop:
+		default:
+			stop();
+			closeDoors();
+			break;
+		}
+	}
+
+	void centerAutoProgram() {
+		long driveTime = 0;
+
+		switch (autoStep) {
+		case Start:
+			break;
+
+		case Step1:
+			try {
+				driveTime = Long.parseLong(SmartDashboard.getString("Center Drive Time ", "bad"));
+			} catch (NumberFormatException e) {
+				driveTime = centerDriveTime;
+			}
+			driveForward( 0.05, driveTime );
+			break;
+
+		case Stop:
+		default:
+			stop();
+			break;
+		}
+	}
+
+	void rightAutoProgram() {
+		long driveTime = 0;
+
+		switch (autoStep) {
+		case Start:
+			break;
+
+		case Step1:
+			try {
+				driveTime = Long.parseLong(SmartDashboard.getString("Before Turn Time ", "bad"));
+			} catch (NumberFormatException e) {
+				driveTime = beforeTurnTime;
+			}
+
+			driveForward(0.3, driveTime);
+			break;
+
+		case Step2:
+			turnLeft(0.10, 90);
+			watchForReflectiveStrips = true;
+			break;
+
+		case Step3:
+			try {
+				driveTime = Long.parseLong(SmartDashboard.getString("After Turn Time ", "bad"));
+			} catch (NumberFormatException e) {
+				driveTime = afterTurnTime;
+			}
+
+			driveForward(0.2, driveTime);
+			break;
+
+		case Step4:
+			pause(1000);
+			openDoors();
+			break;
+
+		case Step5:
+			driveReverse(0.3, 1000);
+
+		case Stop:
+		default:
+			stop();
+			closeDoors();
+			break;
+		}
+	}
+
+	void doStep() {
+
+		double x = 0;
+		double y = 0;
+		double t = 0;
+
+		switch (driveState) {
+
+		case Forward:
+			y = targetSpeed;
+			//			t = -angularDistance * Kp;
+			break;
+
+		case Reverse:
+			y = -targetSpeed;
+			//			t = -angularDistance * Kp;
+			break;
+
+		case Left:
+			x = targetSpeed;
+			//			t = -angularDistance * Kp;
+			break;
+
+		case Right:
+			x = -targetSpeed;
+			//			t = -angularDistance * Kp;
+			break;
+
+		case Turn:
+			t = getTurningSpeedFromAngularDistance(angularDistance);
+			break;
+
+		case Stopped:
+		default:
+			break;
+		}
+
+		baseDriveLevel = SmartDashboard.getNumber("Base Drive Level ", 0);
+		baseTwistLevel = SmartDashboard.getNumber("Base Twist Level ", 0);
+
+		baseDriveLevel = Math.max( 0.0, baseDriveLevel );
+		baseDriveLevel = Math.min( 1.0, baseDriveLevel );
+
+		baseTwistLevel = Math.max( 0.0, baseTwistLevel );
+		baseTwistLevel = Math.min( 1.0, baseTwistLevel );
+
+		if (x < 0) {
+			x = ((1.0 - baseDriveLevel) * x) - baseDriveLevel;
+		} else if (x > 0) {
+			x = ((1.0 - baseDriveLevel) * x) + baseDriveLevel;
+		}
+
+		if (y < 0) {
+			y = ((1.0 - baseDriveLevel) * y) - baseDriveLevel;
+		} else if (y > 0) {
+			y = ((1.0 - baseDriveLevel) * y) + baseDriveLevel;
+		}
+
+		if (t < 0) {
+			t = ((1.0 - baseTwistLevel) * t) - baseTwistLevel;
+		} else if (t > 0) {
+			t = ((1.0 - baseTwistLevel) * t) + baseTwistLevel;
+		}
+
+		SmartDashboard.putNumber("X ", x);
+		SmartDashboard.putNumber("Y ", y);
+		SmartDashboard.putNumber("T ", t);
+
+		
+			driveTrain.arcadeDrive(-x, t);
+	}
+
+	void initializeForNextStep() {
+		targetTime = 0;
+		targetSpeed = 0;
+		targetAngle = 0;
+		targetTurningSpeed = 0;
+		watchForReflectiveStrips = false;
+		initAutoStep = true;
+	}
+
+	void driveForward(double speed, long time) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Forward;
+			targetSpeed = speed;
+			targetTime = getTargetTime(time);
+			targetAngle = currentAngle;
+		}
+	}
+
+	void driveReverse(double speed, long time) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Reverse;
+			targetSpeed = speed;
+			targetTime = getTargetTime(time);
+			targetAngle = currentAngle;
+		}
+	}
+
+	void slideLeft(double speed, long time) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Left;
+			targetSpeed = speed;
+			targetTime = getTargetTime(time);
+			targetAngle = currentAngle;
+		}
+	}
+
+	void slideRight(double speed, long time) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Right;
+			targetSpeed = speed;
+			targetTime = getTargetTime(time);
+			targetAngle = currentAngle;
+		}
+	}
+
+	void turnLeft(double speed, double angle) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Turn;
+			targetTurningSpeed = speed;
+			targetAngle = getTargetAngle(currentAngle, -angle);
+		}
+	}
+
+	void turnRight(double speed, double angle) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Turn;
+			targetTurningSpeed = speed;
+			targetAngle = getTargetAngle(currentAngle, angle);
+		}
+	}
+
+	void stop() {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Stopped;
+			targetTime = 0;
+			targetSpeed = 0;
+			targetAngle = 0;
+			targetTurningSpeed = 0;
+		}
+	}
+
+	void pause( long time ) {
+		if (initAutoStep) {
+			initAutoStep = false;
+			driveState = DriveState.Forward;
+			targetTime = getTargetTime(time);
+			targetSpeed = 0;
+			targetAngle = 0;
+			targetTurningSpeed = 0;
+		}
+	}
+
+	long getTargetTime(long delta) {
+		return System.currentTimeMillis() + delta;
+	}
+
+	double getTargetAngle(double currentAngle, double delta) {
+
+		return (currentAngle + delta) % 360;
+	}
+
+	double getAngularDistanceFromTarget(double currentAngle, double targetAngle) {
+
+		double delta = targetAngle - currentAngle;
+
+		if (delta > 180)
+			delta -= 360;
+		if (delta < -180)
+			delta += 360;
+
+		return delta;
+	}
+
+	double getTurningSpeedFromAngularDistance(double delta) {
+
+		double t = (delta < 0) ? targetTurningSpeed : -targetTurningSpeed;
+
+		double d = Math.abs(delta);
+
+		if (d < 10) {
+			t = t * (d / 10);
+		}
+
+		return t;
+	}
+
+	void openDoors() {
+	}
+
+	void closeDoors() {
+	}
 
 	/**
 	 * This function is called periodically during test mode
